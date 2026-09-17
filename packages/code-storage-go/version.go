35610abbb49bfd65c9bcf684785c3017a5769020
Ian Ownbey <imownbey@gmail.com>
2026-09-17 15:34:40 -0400

    Bump all Code Storage SDKs to 1.17.1 (#63)
    Co-authored-by: Ian Ownbey <ian@pierre.co>

--- packages/code-storage-go/version.go
+++ packages/code-storage-go/version.go
@@ -3,7 +3,7 @@ package storage
 const (
 	PackageName = "code-storage-go-sdk"
 	// PackageVersion is set from .version by scripts/sync_versions.py.
-	PackageVersion = "1.17.0"
+	PackageVersion = "1.17.1"
 )
 
 func userAgent() string {
