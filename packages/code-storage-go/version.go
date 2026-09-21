66cf2664a0cd3d4f453527daa75b6ffae8095366
rkdud007 <pia@pierre.co>
2026-09-21 22:44:03 +0200

    chore(release): prepare 1.18.0-beta.0

--- packages/code-storage-go/version.go
+++ packages/code-storage-go/version.go
@@ -3,7 +3,7 @@ package storage
 const (
 	PackageName = "code-storage-go-sdk"
 	// PackageVersion is set from .version by scripts/sync_versions.py.
-	PackageVersion = "1.17.1"
+	PackageVersion = "1.18.0-beta.0"
 )
 
 func userAgent() string {
